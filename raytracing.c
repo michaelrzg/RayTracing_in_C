34dd6cbb3997107605e82145856e76351e2fe780
michaelrzg <michaelrizig@gmail.com>
2025-03-05 01:26:31 -0500

    lightbulb is now dragable

--- raytracing.c
+++ raytracing.c
@@ -1,20 +1,33 @@
+// raytracing in C
+// Michael Rizig
+// Using new SDL3 lib released Jan 2025
 
 #define SDL_MAIN_USE_CALLBACKS
 #include <SDL3/SDL_main.h>
 #include <SDL3/SDL.h>
 #include <math.h>
+
 #define WIDTH 800
 #define HEIGHT 600
 #define COLOR_WHITE 0xffffffff
+// create window and renderer (from SDL docs)
 SDL_Window *window;
 SDL_Renderer *renderer;
+// track if mouse is draggin and current drag location
+bool isDragging = false;
+int dragStartX, dragStartY;
 
+// struct of a circle
 struct Circle
 {
     double x;
     double y;
     double radius;
 };
+// create the primary circle on the screen
+struct Circle lightbulb = {100, 100, 40};
+
+// this function draws a passed circle on the screen
 void draw_circle(struct Circle circle)
 {
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // Set the draw color to white
@@ -35,7 +48,8 @@ void draw_circle(struct Circle circle)
         }
     }
 }
-// free up memory by deconstructing objects on quit
+
+// this function frees up memory by deconstructing objects on quit
 void SDL_AppQuit(void *appstate, SDL_AppResult result)
 {
     SDL_DestroyRenderer(renderer);
@@ -44,21 +58,20 @@ void SDL_AppQuit(void *appstate, SDL_AppResult result)
     window = NULL;
     SDL_QuitSubSystem(SDL_INIT_VIDEO);
 }
-void draw_lightsource()
-{
-    struct Circle circle = {100, 100, 40};
-    draw_circle(circle);
-}
+
 void render()
 {
+    // set background color here
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
-    draw_lightsource();
+    // draw light sorce (struct Circle)
+    draw_circle(lightbulb);
     SDL_RenderPresent(renderer);
 }
 
 SDL_AppResult SDL_AppIterate(void *appstate)
 {
+    SDL_Delay(10);
     render();
     return SDL_APP_CONTINUE;
 }
@@ -68,6 +81,37 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
     if (event->type == SDL_EVENT_QUIT)
     {
         return SDL_APP_SUCCESS;
+    }
+
+    if (event->type == SDL_EVENT_MOUSE_BUTTON_DOWN)
+    {
+        if (event->button.button == SDL_BUTTON_LEFT)
+        {
+            isDragging = true;
+            dragStartX = event->button.x;
+            dragStartY = event->button.y;
+        }
+    }
+
+    if (event->type == SDL_EVENT_MOUSE_BUTTON_UP)
+    {
+        if (event->button.button == SDL_BUTTON_LEFT)
+        {
+            isDragging = false;
+        }
+    }
+
+    if (event->type == SDL_EVENT_MOUSE_MOTION)
+    {
+        if (isDragging)
+        {
+            int x = event->motion.x;
+            int y = event->motion.y;
+            SDL_Log("Dragging: x=%d, y=%d", x, y);
+
+            lightbulb.x = x;
+            lightbulb.y = y;
+        }
     }
     return SDL_APP_CONTINUE;
 }
